df418410158f0b981192aa680f583a3316a49cdf
naituw <naituw@gmail.com>
2013-05-24 18:09:03 +0800

    更改WeiboComposition结构为protocol，方便使用
    例如需要使其为NSDocument时

---
+++ WeiboComposition.h
@@ -0,0 +1,13 @@
+//
+//  WeiboComposition.h
+//  Weibo
+//
+//  Created by Wutian on 13-5-23.
+//  Copyright (c) 2013年 Wutian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol WeiboComposition <NSObject>
+
+@end
